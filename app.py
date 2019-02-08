e386fc2c1a9e605ba2aa1c478341a5c742af3135
Lillian <inureye1@gmail.com>
2019-02-08 17:07:58 +0800

    dgddddd

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '很抱歉，您說什麼'
+    
 
     if '給我貼圖' in msg:
         sticker_message = StickerSendMessage(
@@ -47,9 +47,9 @@ def handle_message(event):
 
         line_bot_api.reply_message(
         event.reply_token,
-        sticker_message)
-        return
-
+        sticker_message) #回覆貼圖
+        return #這個函式結束
+ 
     if msg in ['hi', 'Hi']:
         r = '嗨'
     elif msg == '你吃飯了嗎':
@@ -58,7 +58,8 @@ def handle_message(event):
         r = '我是機器人'
     elif '訂位' in msg:
         r = '您想訂位，是嗎'
-   
+   else:
+        r = msg
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
